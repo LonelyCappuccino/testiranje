effb0c6a720283f41571f1d326eaf74f391b83f0
LonelyCappuccino <Golob@Klemens-Mac.local>
2013-09-13 16:15:30 +0200

    1.0

---
+++ teest/teest/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  teest
+//
+
+//  Copyright (c) 2013 Tomaz Golob. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ teest/teest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  teest
+//
+
+//  Copyright (c) 2013 Tomaz Golob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
